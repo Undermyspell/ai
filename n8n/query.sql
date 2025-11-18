a461a189df499d2623285c258b5b38e1e06e5466
Michael Kaiser <19801112+Undermyspell@users.noreply.github.com>
2025-11-18 15:45:09 +0100

    refine query

--- n8n/query.sql
+++ n8n/query.sql
@@ -1,18 +1,33 @@
-WITH thursdays AS (
-    SELECT COUNT(*) AS thursday_count
-    FROM generate_series(
-        date_trunc('year', current_date)::date,
+WITH user_thursdays AS (
+    SELECT 
+        u."userId",
+        COUNT(*) AS thursday_count
+    FROM public.users u
+    CROSS JOIN LATERAL generate_series(
+        /* start date */
+        COALESCE(u."startDate", date_trunc('year', current_date))::date,
+        /* end date */
         current_date,
         interval '1 day'
     ) AS d(day)
     WHERE EXTRACT(ISODOW FROM d.day) = 4
+    GROUP BY u."userId"
 )
 SELECT 
-	Count(*) as away_count,
-	t.thursday_count - COUNT(*) as attendance_count,
-	ROUND(COUNT(*)::numeric / t.thursday_count * 100, 2) AS attend_percentage,
-	a."userId" as user_id, 
-	a."userName" as user_name
-FROM public.stammtisch_abwesenheit a
-CROSS JOIN thursdays t 
-group by a."userId", a."userName", t.thursday_count;
+    COUNT(a."userId") AS away_count,
+    ut.thursday_count - COUNT(a."userId") AS attendance_count,
+    ROUND(
+        (ut.thursday_count - COUNT(a."userId")::numeric) 
+        / ut.thursday_count * 100,
+        2
+    ) AS attend_percentage,
+    u."userId" AS user_id,
+    u."userName" AS user_name,
+	u."startDate"::text AS start_date
+FROM public.users u
+LEFT JOIN public.stammtisch_abwesenheit a 
+    ON u."userId" = a."userId"
+JOIN user_thursdays ut
+    ON ut."userId" = u."userId"
+GROUP BY u."userId", u."userName", ut.thursday_count
+ORDER BY attendance_count DESC, attend_percentage DESC;
